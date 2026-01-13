e612944739689cabcd8b86f613808c55eaf78aa3
Josué Ginez <ginezivan409@gmail.com>
2026-01-13 09:02:05 -0600

    Clase 13-01-2026

--- laboratorio-01.sql
+++ laboratorio-01.sql
@@ -4,9 +4,9 @@ CREATE USER 'jginez'@'127.0.0.1';
 CREATE USER 'jginez'@'::1';
 CREATE USER 'jginez'@'172.234.214.2';
 
-
 CREATE USER 'jginez'@'localhost' IDENTIFIED BY  'jginez';
 
+//eliminar usuario
 DROP USER 'jginez'@'localhost';
 
 #DAR PERMISOS           BD.tables
@@ -17,13 +17,15 @@ GRANT ALL PRIVILEGES ON metro_cdmx.*;
 
 GRANT ALL PRIVILEGES ON *.* TO 'jginez'@'localhost';
 
+//Visualizar el usuario que se esta utilizando
 SELECT user();
 
 # root
-1. instala - unica vez
-2. crear un usuario admin
+1. instala - unica vez;
+2. crear un usuario admin;
 3. crear roles administrativos, programmer, capturista, etc.
 
+//crear base de datos
 create database ecommerce;
 
 
